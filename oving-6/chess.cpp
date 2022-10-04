f4f0621da1d038d876379fff51a46ca2ad546bf6
Nicolay Roness <nicroness@gmail.com>
2022-10-04 12:48:54 +0200

    Completed task 6

--- oving-6/chess.cpp
+++ oving-6/chess.cpp
@@ -86,13 +86,13 @@ public:
   /// 8x8 squares occupied by 1 or 0 chess pieces
   vector<vector<unique_ptr<Piece>>> squares;
 
-
   function<void(const Piece &piece, const string &from, const string &to)> on_piece_move;
   function<void(const Piece &piece, const string &square)> on_piece_removed;
   function<void(Color color)> on_lost_game;
   function<void(const Piece &piece, const string &from, const string &to)> on_piece_move_invalid;
   function<void(const string &square)> on_piece_move_missing;
 
+  function<void()> after_turn = 0;
 
   /// Move a chess piece if it is a valid move.
   /// Does not test for check or checkmate.
@@ -108,21 +108,25 @@ public:
         if (on_piece_move) {
           on_piece_move(*piece_from, from, to);
         }
+        after_turn();
         auto &piece_to = squares[to_x][to_y];
         if (piece_to) {
           if (piece_from->color != piece_to->color) {
             if (on_piece_removed) {
                 on_piece_removed(*piece_to, to);
             }
-            if (auto king = dynamic_cast<King *>(piece_to.get()))
+            after_turn();
+            if (auto king = dynamic_cast<King *>(piece_to.get())) {
               if (on_lost_game) {
                 on_lost_game(king->color);
               }
+            }
           } else {
             // piece in the from square has the same color as the piece in the to square
             if (on_piece_move_invalid) {
               on_piece_move_invalid(*piece_from, from, to);
             }
+            after_turn();
             return false;
           }
         }
@@ -142,23 +146,54 @@ public:
     }
   }
 
-  void print_board() {
-    int board_size = squares.size();
-    for (auto &row : squares) {
-      print_line(board_size);
-      for (auto &square : row) {
-        std::cout << "| ";
-        if (square) {
-          std::cout << square->to_string();
-        } else {
-          std::cout << " ";
-        }
-        std::cout << ' ';
-      }
-      std::cout << '|' << std::endl;
-    }
-    print_line(board_size);
-  }
+ };
+
+class ChessBoardPrint {
+public:
+    ChessBoardPrint(ChessBoard &board) {
+        board.on_piece_move = [](const ChessBoard::Piece &piece, const string &from, const string &to) {
+          cout << piece.type() << " is moving from " << from << " to " << to << endl;
+        };
+
+        board.on_piece_removed = [](const ChessBoard::Piece &piece, const string &square) {
+          cout << piece.type() << " is being removed from " << square << endl;
+        };
+
+        board.on_lost_game = [](const ChessBoard::Color &color) {
+          if (color == ChessBoard::Color::WHITE) {
+              cout << "white";
+          } else {
+              cout << "black";
+          }
+          cout << " won the game" << endl;
+        };
+
+        board.on_piece_move_invalid = [](const ChessBoard::Piece &piece, const string &from, const string &to) {
+          cout << "can not move " << piece.type() << " from " << from << " to " << to << endl;
+        };
+
+        board.on_piece_move_missing = [](const string &square) {
+          cout << "no piece at " << square << endl;
+        };
+
+        board.after_turn = [this, &board]() {
+          int board_size = board.squares.size();
+          for (auto &row : board.squares) {
+            print_line(board_size);
+            for (auto &square : row) {
+              std::cout << "| ";
+              if (square) {
+              std::cout << square->to_string();
+              } else {
+                std::cout << " ";
+              }
+              std::cout << ' ';
+            }
+          std::cout << '|' << std::endl;
+          }
+        print_line(board_size);
+        };
+    };
 
 private:
   void print_line(int size) {
@@ -167,72 +202,6 @@ private:
     }
     std::cout << std::endl;
   }
-
-};
-
-class ChessBoardPrint {
-public:
-
-    ChessBoard board;
-
-    ChessBoardPrint(ChessBoard &board) : board(board) {
-        board.on_piece_move = [this](const ChessBoard::Piece &piece, const string &from, const string &to) {
-            move_piece(piece, from, to);
-        };
-    };
-
-
-    void move_piece(const ChessBoard::Piece &piece, const string &from, const string &to) {
-        cout << piece.type() << " is moving from " << from << " to " << to << endl;
-    }
-
-    void remove_piece(const ChessBoard::Piece &piece, const string &square) {
-        cout << piece.type() << " is being removed from " << square << endl;
-    }
-
-    void game_lost(ChessBoard::Color color) {
-        if (color == ChessBoard::Color::WHITE) {
-            cout << "White";
-        } else {
-            cout << "Black";
-        }
-        cout << " won the game" << endl;
-    }
-
-    void invalid_move(const ChessBoard::Piece &piece, const string &from, const string &to) {
-        cout << "can not move " << piece.type() << " from " << from << " to " << to << endl;
-    }
-
-    void piece_missing(const string &square) {
-        cout << "no piece at " << square << endl;
-    }
-
-
-  /* void print_board() { */
-  /*   int board_size = board.squares.size(); */
-  /*   for (auto &row : board.squares) { */
-  /*     print_line(board_size); */
-  /*     for (auto &square : row) { */
-  /*       std::cout << "| "; */
-  /*       if (square) { */
-  /*         std::cout << square->to_string(); */
-  /*       } else { */
-  /*         std::cout << " "; */
-  /*       } */
-  /*       std::cout << ' '; */
-  /*     } */
-  /*     std::cout << '|' << std::endl; */
-  /*   } */
-  /*   print_line(board_size); */
-  /* } */
-
-/* private: */
-  /* void print_line(int size) { */
-  /*   for (int i = 0; i < size; i++) { */
-  /*     std::cout << " ---"; */
-  /*   } */
-  /*   std::cout << std::endl; */
-  /* } */
 
 };
 
@@ -249,7 +218,7 @@ int main() {
   board.squares[6][7] = make_unique<ChessBoard::Knight>(ChessBoard::Color::BLACK);
 
   cout << "Initial board:" << endl;
-  board.print_board();
+  board.after_turn();
 
   cout << "Invalid moves:" << endl;
   board.move_piece("e3", "e2");
@@ -259,22 +228,13 @@ int main() {
 
   cout << "A simulated game:" << endl;
   board.move_piece("e1", "e2");
-  board.print_board();
   board.move_piece("g8", "h6");
-  board.print_board();
   board.move_piece("b1", "c3");
-  board.print_board();
   board.move_piece("h6", "g8");
-  board.print_board();
   board.move_piece("c3", "d5");
-  board.print_board();
   board.move_piece("g8", "h6");
-  board.print_board();
   board.move_piece("d5", "f6");
-  board.print_board();
   board.move_piece("h6", "g8");
-  board.print_board();
   board.move_piece("f6", "e8");
-  board.print_board();
   return 0;
 }
